021386222b9ca552c965d2a3d7edebb082a875cb
Blake Haswell <haswell00@gmail.com>
2012-12-27 23:14:41 +1100

    Added comment.

--- src/Pong.js
+++ src/Pong.js
@@ -3,7 +3,10 @@ var Pong = function () {
 };
 
 Pong.prototype.init = function () {
+    
+    // Call gameLoop() every 50ms.
     setInterval(this.gameLoop, 50);
+    
 };
 
 Pong.prototype.gameLoop = function () {
