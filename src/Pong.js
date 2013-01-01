b6c0cf4b1d8f73939722fde5f3c798fe3a230e17
Blake Haswell <haswell00@gmail.com>
2013-01-01 14:18:21 +1100

    Let the paddle object take care of creating the paddles.

--- src/Pong.js
+++ src/Pong.js
@@ -9,8 +9,38 @@ Pong.prototype.init = function () {
     playingCourt.init();
     
     // Create paddles.
-    $('<div class="paddle" id="paddle_playerOne"></div>').appendTo('body');
-    $('<div class="paddle" id="paddle_playerTwo"></div>').appendTo('body');
+    var paddles = [
+        new Paddle({
+            name: 'playerOne',
+            height: 128,
+            position: {
+                x: 88,
+                y: 240
+            },
+            controls: {
+                up: 97,
+                down: 122
+            }
+        }),
+        
+        new Paddle({
+            name: 'playerTwo',
+            height: 128,
+            position: {
+                x: 552,
+                y: 240
+            },
+            controls: {
+                up: 107,
+                down: 109
+            }
+        })
+    ];
+    
+    // Initialise the paddles.
+    $.each(paddles, function (index, paddle) {
+        paddle.init();
+    });
     
     // Call gameLoop() every 50ms.
     setInterval(this.gameLoop, 50);
